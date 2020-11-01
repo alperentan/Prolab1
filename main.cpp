8387a54d37e1bb90051580e38389c9475641906b
alperentan <56079264+alperentan@users.noreply.github.com>
2020-11-01 20:27:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,129 +2,190 @@
 #include <stdlib.h>
 #include <math.h>
 #include <graphics.h>
-struct point{
+double splinex[1000];
+double spliney[1000];
+struct point   {
 int x;
-int y;
-};
-int get_size(){
+int y;   };
+struct point pnt[6];
+int get_size()   {
     FILE *f=fopen("C:\\Users\\tanal\\Desktop\\den.txt","r");
-
-if(f!=NULL){
+//dosya
+if(f!=NULL)   {
    int i=0;
-   while(!feof(f)){
-
-       if(fgetc(f)!='{'&&fgetc(f)!='}'){
-          i++;
-       }
-   }
+   while(!feof(f))   {
+      if(fgetc(f)==',')   {
+          i++;   }   }
    fclose(f);
-   return i;
-}
-else{
-    printf("File does not exist");
-}
-}
-void readfile(char *coordinate){
+   return i;   }
+else   {
+    printf("File does not exist");   }   }//dosya
+void readfile(char *coordinate)   {
 FILE *f=fopen("C:\\Users\\tanal\\Desktop\\den.txt","r");
 int i=0;
-if(f!=NULL){
-   while(!feof(f)){
+if(f!=NULL)   {
+   while(!feof(f))   {
        char c;
         c=fgetc(f);
-       if(c!='{'&&c!='}'){
+       if(c!='{'&&c!='}')   {
           coordinate[i]=c;
-        i++;
-       }
-   }
-   fclose(f);
-}
-else{
-    printf("File does not exist");
-}
-}
-void parse_coordinate(char *coor,struct point pt[],int size){
-char *say;
+        i++;   }   }
+   fclose(f);   }
+else   {
+    printf("File does not exist");   }   }
+    //dosya
+void parse_coordinate(char *coor,struct point pt[],int size)   {
+    char *say;
     say=strtok(coor," ,");
     pt[0].x=(float)atoi(say);
     say = strtok (NULL, " ,");
     pt[0].y=(float)atoi(say);
-    for (int i = 2; i < size; i+=2) {
+    for (int i = 1; i < size; i++)   {
         say = strtok (NULL, " ,");
-        pt[i/2].x=(float)atoi(say);
+        pt[i].x=(float)atoi(say);
         say = strtok (NULL, " ,");
-        pt[i/2].y=(float)atoi(say);
-    }
-}
-int main()
-{   int size=get_size();
-    char coordinate[size];
-    struct point pt[size/2];
+        pt[i].y=(float)atoi(say);   }   }
+int main()   {
+    int size=get_size()/2+1;
+    char coordinate[size*5];
+    struct point pt[size];
     readfile(coordinate);
     //printf("%s",coordinate);
     parse_coordinate(coordinate,pt,size);
     float x,y,x2,y2;
-    for(int i=0;i<size/2;i++){
-       printf("( %d , %d )",pt[i].x,pt[i].y);
-    }
+    for(int i=0;i<size;i++)   {
+       printf("( %d , %d )",pt[i].x,pt[i].y);   }
     printf("\n");
     float mer_x=0;
     float mer_y=0;
     float yaricap=0;
-    if(size==2){
-        printf("Cemberin merkezi=(%d %d)\nCemberin yaricapi=0(nokta)\n",pt[0].x,pt[0].y);
-    }
-    else if(size==4)   {
+    if(size==1)   {
+        printf("Cemberin merkezi=(%d %d)\nCemberin yaricapi=0(nokta)\n",pt[0].x,pt[0].y);   }
+    else if(size==2)   {
     mer_x=(pt[0].x+pt[1].x)/2.0;
     mer_y=(pt[0].y+pt[1].y)/2.0;
     yaricap=sqrt(pow(pt[1].x-mer_x,2)+pow(pt[1].y-mer_y,2));
-    printf("Cemberin merkezi=(%f %f)\nCemberin yaricapi=%f\n",mer_x,mer_y,yaricap);   }
-
-    else{
+    printf("Cemberin merkezi=(%.2f %f)\nCemberin yaricapi=%.2f\n",mer_x,mer_y,yaricap);   }
+        else   {
     float ebu=sqrt(pow(pt[1].x-pt[0].x,2)+pow(pt[1].y-pt[0].y,2));
-    for(int i=0;i<size/2;i++)   {
-    for(int j=i+1;j<size/2;j++)   {
+    for(int i=0;i<size;i++)   {
+    for(int j=i+1;j<size;j++)   {
         if(sqrt(pow(pt[j].x-pt[i].x,2)+pow(pt[j].y-pt[i].y,2))>ebu){
             ebu=sqrt(pow(pt[j].x-pt[i].x,2)+pow(pt[j].y-pt[i].y,2));
             x=pt[i].x;
             y=pt[i].y;
             x2=pt[j].x;
-            y2=pt[j].y;}
-        }   }
-    printf("Iki nokta arasi en buyuk uzaklik=%f\nEn uzak noktalarin koordinatlari=(%f,%f)-(%f,%f)\n",ebu,x,y,x2,y2);
+            y2=pt[j].y;   }   }   }
+    printf("Iki nokta arasi en buyuk uzaklik=%.2f\nEn uzak noktalarin koordinatlari=(%.2f,%.2f)-(%.2f,%.2f)\n",ebu,x,y,x2,y2);
     float x3,y3,enuzakucuncuuzunluk=0;
-    for(int i=0;i<size/2;i++){
+    for(int i=0;i<size;i++)   {
     if(sqrt(pow(pt[i].x-x,2)+pow(pt[i].y-y,2))+sqrt(pow(pt[i].x-x2,2)+pow(pt[i].y-y2,2))>enuzakucuncuuzunluk){
         enuzakucuncuuzunluk=sqrt(pow(pt[i].x-x,2)+pow(pt[i].y-y,2))+sqrt(pow(pt[i].x-x2,2)+pow(pt[i].y-y2,2));
         x3=pt[i].x;
-        y3=pt[i].y;}}
-    printf("(%f,%f)-(%f,%f)'ya en uzak ucuncu nokta=(%f,%f)\n",x,y,x2,y2,x3,y3);
+        y3=pt[i].y;   }   }
+    printf("(%.2f,%.2f)-(%.2f,%.2f)'ya en uzak ucuncu nokta=(%.2f,%.2f)\n",x,y,x2,y2,x3,y3);
     mer_x=(x2+x)/2;
     mer_y=(y2+y)/2;
     yaricap=sqrt(pow(x-mer_x,2)+pow(y-mer_y,2));
     if((sqrt(pow(x3-mer_x,2)+pow(y3-mer_y,2)))>yaricap)   {
-    float egri1=0;
-    float egri2=0;
-    float sabit1=0;
-    float sabit2=0;
-    float orta1x=0;
-    float orta1y=0;
-    float orta2x=0;
-    float orta2y=0;
-    egri1=(y2-y)/(x2-x);
-    egri2=(y2-y3)/(x2-x3);
-    orta1x=(x+x2)/2;
-    orta1y=(y+y2)/2;
-    orta2x=(x2+x3)/2;
-    orta2y=(y2+y3)/2;
-
-    sabit1=orta1y+(1/egri1)*orta1x;
-    sabit2=orta2y+(1/egri2)*orta2x;
-    //printf("%f %f\n",sabit1,sabit2);
-    mer_x=(sabit2-sabit1)/(((x3-x2)/(y2-y3))+((x2-x)/(y-y2)));
-    mer_y=(-1/egri2)*mer_x+sabit2;
+    float c23=0;
+    float c12=0;
+    float mb_norm=0;
+    float ma_norm=0;
+    c23=((y2+y3)/2)-(((x2+x3)/2)*((x3-x2)/(y2-y3)));
+    c12=((y+y2)/2)-(((x+x2)/2)*((x-x2)/(y2-y)));
+    ma_norm=((x-x2)/(y2-y));
+    mb_norm=((x3-x2)/(y2-y3));
+    mer_x=(c23-c12)/(ma_norm-mb_norm);
+    mer_y=(ma_norm*mer_x)+c12;
     yaricap=sqrt(pow(x-mer_x,2)+pow(y-mer_y,2));
-    printf("Cemberin merkezi= x:%f y:%f\nCemberin yaricapi=%f\n",mer_x,mer_y,yaricap);   }
+    printf("Cemberin merkezi= x:%.2f y:%.2f\nCemberin yaricapi=%.2f\n",mer_x,mer_y,yaricap);   }
     else   {
-        printf("Cemberin merkezi= x:%f y:%f\nCemberin yaricapi=%f\n",mer_x,mer_y,yaricap);   }   }
+        printf("Cemberin merkezi= x:%.2f y:%f\nCemberin yaricapi=%.2f\n",mer_x,mer_y,yaricap);   }   }
+        int gd= DETECT,gm;
 
-    return 0;   }
+initwindow(600,600);
+settextstyle(SMALL_FONT,HORIZ_DIR,4);
+char merkez[50];
+sprintf(merkez,"Merkez=(%.2f,%.2f)",mer_x,mer_y);
+char yaricap_[50];
+sprintf(yaricap_,"Yaricap=(%.2f)",yaricap);
+outtextxy(0,0,merkez);
+outtextxy(0,10,yaricap_);
+moveto(250,250);
+lineto(250,0);
+lineto(250,500);
+moveto(250,250);
+lineto(500,250);
+lineto(0,250);
+moveto(0,0);
+outtextxy(250,250,"0");
+outtextxy(500,255,"25");
+outtextxy(505,245,"x");
+outtextxy(235,0,"25");
+outtextxy(230,500,"-25");
+outtextxy(248,501,"y");
+outtextxy(0,255,"-25");
+outtextxy(270,255,"2");
+outtextxy(290,255,"4");
+outtextxy(310,255,"6");
+outtextxy(330,255,"8");
+outtextxy(350,255,"10");
+outtextxy(370,255,"12");
+outtextxy(390,255,"14");
+outtextxy(410,255,"16");
+outtextxy(430,255,"18");
+outtextxy(450,255,"20");
+outtextxy(470,255,"22");
+outtextxy(234,30,"22");
+outtextxy(234,50,"20");
+outtextxy(235,70,"18");
+outtextxy(235,90,"16");
+outtextxy(235,110,"14");
+outtextxy(235,130,"12");
+outtextxy(235,150,"10");
+outtextxy(235,170,"8");
+outtextxy(235,190,"6");
+outtextxy(235,210,"4");
+outtextxy(235,230,"2");
+outtextxy(235,270,"-2");
+outtextxy(235,290,"-4");
+outtextxy(235,310,"-6");
+outtextxy(235,330,"-8");
+outtextxy(230,350,"-10");
+outtextxy(230,370,"-12");
+outtextxy(230,390,"-14");
+outtextxy(230,410,"-16");
+outtextxy(230,430,"-18");
+outtextxy(230,450,"-20");
+outtextxy(230,470,"-22");
+outtextxy(230,255,"-2");
+outtextxy(210,255,"-4");
+outtextxy(190,255,"-6");
+outtextxy(170,255,"-8");
+outtextxy(150,255,"-10");
+outtextxy(130,255,"-12");
+outtextxy(110,255,"-14");
+outtextxy(90,255,"-16");
+outtextxy(70,255,"-18");
+outtextxy(50,255,"-20");
+outtextxy(30,255,"-22");
+circle(250+(mer_x*10),250-(mer_y*10),yaricap*10);
+moveto(250+(mer_x*10),250-(mer_y*10));
+lineto(250+(x2*10),250-(y2*10));
+setcolor(9);
+for(int i=0;i<size;i++)   {
+circle(250+(pt[i].x)*10,250-(pt[i].y)*10,2);
+floodfill(250+(pt[i].x)*10,250-(pt[i].y)*10,9);
+char koord[50];
+sprintf(koord,"(%d,%d)",pt[i].x,pt[i].y);
+outtextxy(252+(pt[i].x)*10,252-(pt[i].y)*10,koord);   }
+circle(250+(mer_x)*10,250-(mer_y)*10,2);
+floodfill(250+(mer_x)*10,250-(mer_y)*10,9);
+char merkezkoord[50];
+sprintf(merkezkoord,"(%.1f,%.1f)",mer_x,mer_y);
+outtextxy(213+(mer_x*10),240-(mer_y*10),merkezkoord);
+getch();
+closegraph();
+
+return 0;   }
